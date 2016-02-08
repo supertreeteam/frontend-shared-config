07c5ccfa02a96413a822a723a80184a5f5e33656
Lachlan Priest <lachlanpriest@gmail.com>
2016-02-09 01:03:19 +0800

    Add return to afterInstall

--- blueprints/supertree-shared-config/index.js
+++ blueprints/supertree-shared-config/index.js
@@ -1,46 +1,50 @@
 'use strict';
 
+var RSVP = require('rsvp');
+
 module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    this.addPackageToProject('broccoli-jscs', '1.2.3');
-    this.addPackageToProject('git-repo-info', '^1.0.4');
-    this.addPackageToProject('ember-analytics', '^0.0.4');
-    this.addPackageToProject('ember-cli-mirage', '0.2.0-beta.3');
-    this.addPackageToProject('ember-cli-blanket', '0.4.0');
-    this.addPackageToProject('autoprefixer', '^6.0.3');
-    this.addPackageToProject('broccoli-jscs', '1.2.2');
-    this.addPackageToProject('configstore', '^1.4.0');
-    this.addPackageToProject('cssnano', '^3.2.0');
-    this.addPackageToProject('ember-browserify', '^1.1.4');
-    this.addPackageToProject('ember-cli-font-awesome', '1.4.0');
-    this.addPackageToProject('ember-cli-mirage', '0.2.0-beta.3');
-    this.addPackageToProject('ember-cli-moment-shim', '1.0.0');
-    this.addPackageToProject('ember-cli-postcss-fixed', '0.0.6');
-    this.addPackageToProject('ember-cli-select-box', '0.1.9');
-    this.addPackageToProject('ember-i18n', '4.2.0');
-    this.addPackageToProject('ember-moment', '6.0.0');
-    this.addPackageToProject('ember-qunit', '0.4.19');
-    this.addPackageToProject('ember-simple-auth', '1.0.1');
-    this.addPackageToProject('ember-sinon', '^0.3.0');
-    this.addPackageToProject('ember-validations', '^2.0.0-alpha.4');
-    this.addPackageToProject('liquid-fire', '0.23.0');
-    this.addPackageToProject('markdown-code-highlighting', '0.0.6');
-    this.addPackageToProject('moment', '2.11.1');
-    this.addPackageToProject('moment-timezone', '0.5.0');
-    this.addPackageToProject('postcss-cssnext', '2.4.0');
-    this.addPackageToProject('postcss-normalize', '^0.1.2');
-    this.addPackageToProject('postcss-scss', '^0.1.3');
-    this.addPackageToProject('precss', '^1.2.3');
-    this.addBowerPackageToProject('blanket', '~1.1.5');
-    this.addBowerPackageToProject('ember-simple-auth', '0.8.0');
-    this.addBowerPackageToProject('Faker', '~3.0.0');
-    this.addBowerPackageToProject('font-awesome', '~4.5.0');
-    this.addBowerPackageToProject('highlightjs', '~9.1.0');
-    this.addBowerPackageToProject('lodash', '~3.7.0');
-    this.addBowerPackageToProject('marked', '~0.3.5');
-    this.addBowerPackageToProject('pretender', '~0.10.1');
-    this.addBowerPackageToProject('sinonjs', '^1.17.1');
+    return RSVP.all([
+      this.addPackagesToProject([
+        { name: 'autoprefixer',               target: '^6.0.3' },
+        { name: 'broccoli-jscs',              target: '1.2.2' },
+        { name: 'broccoli-jscs',              target: '1.2.2' },
+        { name: 'configstore',                target: '^1.4.0' },
+        { name: 'cssnano',                    target: '^3.2.0' },
+        { name: 'ember-browserify',           target: '^1.1.4' },
+        { name: 'ember-cli-font-awesome',     target: '1.4.0' },
+        { name: 'ember-cli-mirage',           target: '0.2.0-beta.3' },
+        { name: 'ember-cli-moment-shim',      target: '1.0.0' },
+        { name: 'ember-cli-postcss-fixed',    target: '0.0.6' },
+        { name: 'ember-cli-select-box',       target: '0.1.9' },
+        { name: 'ember-i18n',                 target: '4.2.0' },
+        { name: 'ember-moment',               target: '6.0.0' },
+        { name: 'ember-qunit',                target: '0.4.19' },
+        { name: 'ember-simple-auth',          target: '1.0.1' },
+        { name: 'ember-sinon',                target: '^0.3.0' },
+        { name: 'ember-validations',          target: '^2.0.0-alpha.4' },
+        { name: 'git-repo-info',              target: '^1.0.4' },
+        { name: 'liquid-fire',                target: '0.23.0' },
+        { name: 'markdown-code-highlighting', target: '0.0.6' },
+        { name: 'moment',                     target: '2.11.1' },
+        { name: 'moment-timezone',            target: '0.5.0' },
+        { name: 'postcss-cssnext',            target: '2.4.0' },
+        { name: 'postcss-normalize',          target: '^0.1.2' },
+        { name: 'postcss-scss',               target: '^0.1.3' },
+        { name: 'precss',                     target: '^1.2.3' }
+      ]),
+      this.addBowerPackagesToProject([
+        { name: 'ember-simple-auth', target: '0.8.0' },
+        { name: 'Faker',             target: '~3.0.0' },
+        { name: 'font-awesome',      target: '~4.5.0' },
+        { name: 'highlightjs',       target: '~9.1.0' },
+        { name: 'lodash',            target: '~3.7.0' },
+        { name: 'marked',            target: '~0.3.5' },
+        { name: 'pretender',         target: '~0.10.1' },
+        { name: 'sinonjs',           target: '^1.17.1' }
+      ])
+    ]);
   }
 };
